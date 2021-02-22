f2bdc1e658570f20b16be51e53fe78e3fb48af62
Paulo Garcia Arceno <paulofs321@gmail.com>
2021-02-22 19:41:15 +0800

    Remove constructMessage function, change message object to reminder object, add constructReminder method inside the reminder object, finish README.md

--- README.md
+++ README.md
@@ -1,1 +1,53 @@
-# Mixed Messages
+# **Mixed Messages**
+
+A Javascript console app that generates random chore and healthy lifestyle reminders.
+
+---
+
+## Introduction
+
+This is a variation of the Mixed Messages Portfolio project set by Codeacademy. It's designed to provide random chore and healthy lifestyle reminders to the user.
+
+## Table of Contents
+
+* [Features] (#features)
+* [Technologies] (#technologies)
+* [Setup] (#setup)
+* [Usage] (#usage)
+* [Output] (#output)
+
+## Features
+
+Prints to the console random chore and healthy lifestyle reminders to the user.
+
+* Chore reminders include doing the dishes, doing the laundry, feeding the pets, etc..
+* Healthy lifestyle reminders include working out, eating healthy, going on a vacation, etc..
+
+## Technologies
+
+* Javascript
+* Node.js
+* Code Editor
+
+## Setup
+
+To be able to run this script, [you need to setup Node.js locally in your machine.](https://www.codecademy.com/articles/setting-up-node-locally)
+
+## Usage
+
+Once you have Node.js set up, run the script.
+```
+...\your-script-directory> node script.js
+```
+
+## Output
+
+After you run the script, messages should be printed in the terminal similar to the one below,
+```
+...\your-script-directory> node script.js
+CHORE REMINDER:
+Did you do the laundry this morning?
+
+LIFESTYLE REMINDER:
+Have you been working out this year?
+```
--- script.js
+++ script.js
@@ -22,58 +22,53 @@ const messageComponent = {
     }
 };
 
-//function that will construct the message, returns a message object
-const constructMessage = () => {
-    const message = {
-        choreMessage: '',
-        lifestyleMessage: '',
-        displayChoreMessage() {
-            console.log(`CHORE REMINDER:\n${this.choreMessage}\n`);
-        },
-        displayLifestyleMessage() {
-            console.log(`LIFESTYLE REMINDER:\n${this.lifestyleMessage}\n`);
-        },
-        displayBothMessages() {
-            this.displayChoreMessage();
-            this.displayLifestyleMessage();      
+const reminder = {
+    choreReminder: '',
+    lifestyleReminder: '',
+    displayChoreMessage() {
+        console.log(`CHORE REMINDER:\n${this.choreMessage}\n`);
+    },
+    displayLifestyleMessage() {
+        console.log(`LIFESTYLE REMINDER:\n${this.lifestyleMessage}\n`);
+    },
+    displayBothMessages() {
+        this.displayChoreMessage();
+        this.displayLifestyleMessage();      
+    },
+    constructReminders() {
+        //construct message for daily and lifestyle reminders
+        for (let i = 0; i < messageComponent.firstComp.length; i++) {
+            let compValRandomIdx;
+            let secondCompVal;
+            let thirdCompVal;
+
+            switch (messageComponent.firstComp[i]) {
+                case 'Did you': 
+                    const secondCompKeys = Object.keys(messageComponent.secondComp['Did you']);
+                    const compKeyRandomIdx = Math.floor(Math.random() * secondCompKeys.length);
+                    const didYouSecondComp = messageComponent.secondComp['Did you'][secondCompKeys[compKeyRandomIdx]];      //randomly pick between daily or weekly keys  
+                    compValRandomIdx = Math.floor(Math.random() * didYouSecondComp.length);
+                    secondCompVal = didYouSecondComp[compValRandomIdx];     //randomly selects a value from one of the properties of 'Did you' of secondComp
+                    
+                    const didYouThirdComp = messageComponent.thirdComp['Did you'][secondCompKeys[compKeyRandomIdx]];    //third comp key stays the same as second comp
+                    compValRandomIdx = Math.floor(Math.random() * didYouThirdComp.length);
+                    thirdCompVal = didYouThirdComp[compValRandomIdx];   //randomly selects a value from one of the properties of 'Did you' of thirdComp
+
+                    reminder.choreMessage = messageComponent.firstComp[i] + ' ' + secondCompVal + ' ' + thirdCompVal + '?';      //construct chore message
+                    break;
+                case 'Have you':
+                    compValRandomIdx = Math.floor(Math.random() * messageComponent.secondComp['Have you'].length);
+                    secondCompVal = messageComponent.secondComp['Have you'][compValRandomIdx];
+
+                    compValRandomIdx = Math.floor(Math.random() * messageComponent.thirdComp['Have you'].length);
+                    thirdCompVal = messageComponent.thirdComp['Have you'][compValRandomIdx];
+
+                    reminder.lifestyleMessage = messageComponent.firstComp[i] + ' ' + secondCompVal + ' ' + thirdCompVal + '?';
+                    break;
+            }
         }
     }
+}
 
-    //construct message for daily and lifestyle reminders
-    for (let i = 0; i < messageComponent.firstComp.length; i++) {
-        let compValRandomIdx;
-        let secondCompVal;
-        let thirdCompVal;
-
-        switch (messageComponent.firstComp[i]) {
-            case 'Did you': 
-                const secondCompKeys = Object.keys(messageComponent.secondComp['Did you']);
-                const compKeyRandomIdx = Math.floor(Math.random() * secondCompKeys.length);
-                const didYouSecondComp = messageComponent.secondComp['Did you'][secondCompKeys[compKeyRandomIdx]];      //randomly pick between daily or weekly keys  
-                compValRandomIdx = Math.floor(Math.random() * didYouSecondComp.length);
-                secondCompVal = didYouSecondComp[compValRandomIdx];     //randomly selects a value from one of the properties of 'Did you' of secondComp
-                
-                const didYouThirdComp = messageComponent.thirdComp['Did you'][secondCompKeys[compKeyRandomIdx]];    //third comp key stays the same as second comp
-                compValRandomIdx = Math.floor(Math.random() * didYouThirdComp.length);
-                thirdCompVal = didYouThirdComp[compValRandomIdx];   //randomly selects a value from one of the properties of 'Did you' of thirdComp
-
-                message.choreMessage = messageComponent.firstComp[i] + ' ' + secondCompVal + ' ' + thirdCompVal + '?';      //construct chore message
-                break;
-            case 'Have you':
-                compValRandomIdx = Math.floor(Math.random() * messageComponent.secondComp['Have you'].length);
-                secondCompVal = messageComponent.secondComp['Have you'][compValRandomIdx];
-
-                compValRandomIdx = Math.floor(Math.random() * messageComponent.thirdComp['Have you'].length);
-                thirdCompVal = messageComponent.thirdComp['Have you'][compValRandomIdx];
-
-                message.lifestyleMessage = messageComponent.firstComp[i] + ' ' + secondCompVal + ' ' + thirdCompVal + '?';
-                break;
-        }
-    }
-    
-    return message;
-};
-
-const message = constructMessage();
-
-message.displayBothMessages();
+reminder.constructReminders();
+reminder.displayBothMessages();
